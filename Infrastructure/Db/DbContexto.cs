08d51354fe6bd09afb31839bf7a38750143d338a
geraldopereirads <geraldo.pereirads@hotmail.com>
2025-12-17 21:48:47 -0300

    feat: add admin entity and configure EF Core with SQL Server

---
+++ Infrastructure/Db/DbContexto.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehicleHub.Api.Domain.Entity;
+
+namespace VehicleHub.Api.Infrastructure.Db;
+
+public class DbContexto : DbContext
+{
+
+
+    public DbContexto(DbContextOptions<DbContexto> options)
+        : base(options)
+    {
+    }
+    public DbSet<Admin> Admins { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Admin>().HasData(
+            new Admin
+            {
+                Id = -1,
+                Email = "administrador@teste.com",
+                Senha = "123456",
+                Perfil = "Adm"
+            }
+        );
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using VehicleHub.Api.Domain.DTOs;
+using VehicleHub.Api.Infrastructure.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("sqlserver")
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
