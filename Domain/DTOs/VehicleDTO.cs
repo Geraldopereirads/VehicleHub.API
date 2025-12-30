ca498a609755a16535f7b1555263f30f022b1f50
geraldopereirads <geraldo.pereirads@hotmail.com>
2025-12-30 16:03:06 -0300

    feat: add endpoint to create admin users

--- Domain/DTOs/VehicleDTO.cs
+++ Domain/DTOs/VehicleDTO.cs
@@ -1,12 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace VehicleHub.Api.Domain.DTOs;
+﻿namespace VehicleHub.Api.Domain.DTOs;
 
 public record VehicleDTO
 {
     public string Name { get; set; } = default!;
-
     public string Mark { get; set; } = default!;
     public int Year { get; set; } = default!;
 
