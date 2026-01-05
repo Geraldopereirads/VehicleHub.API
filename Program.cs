24b811635bd864b29a45ae36df2bdaf372f9b41b
geraldopereirads <geraldo.pereirads@hotmail.com>
2026-01-05 20:06:28 -0300

    feat: ajusta permissões de rotas para administradores e editores
    Implementa controle de acesso baseado em perfil:
    - Administradores podem acessar todas as rotas
    - Editores acessam apenas rotas explicitamente permitidas

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
